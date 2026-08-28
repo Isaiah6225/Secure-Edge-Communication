bcfaf73fa0a5154a2b9e3b94dd4409ef6e17a02b
Isaiah6225 <isaiahjohnson6225@gmail.com>
2026-08-28 09:23:58 -0400

    Fix for check device.

--- server-code/server/src/database/check_device_db.rs
+++ server-code/server/src/database/check_device_db.rs
@@ -9,10 +9,10 @@ pub fn check_device_db(
     dev_id: [u8; 6], 
     dev_pub: [u8; 33], 
 ) -> Result<(), ServerError> {
-    let mut select_stmt = db_conn.prepare("SELECT mac_address, pub_key, nonce  FROM device WHERE mac_address = :mac_address AND pub_key = pub_key")?;
+    let mut select_stmt = db_conn.prepare("SELECT mac_address, pub_key, nonce  FROM device WHERE mac_address = mac_address AND pub_key = pub_key")?;
     let device_iter = select_stmt.query_map(named_params!{
-       ":mac_address": dev_id,
-       ":pub_key": dev_pub,
+       "mac_address": dev_id,
+       "pub_key": dev_pub,
     }, |row| {
         Ok(DeviceEnrl {
             device_id: row.get(0)?,
